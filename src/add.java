f7596d57044e8dbf300d8fc43de20f56c3c047fe
karthi <karthi@karthi-Lenovo-E41-25>
2022-06-10 10:35:30 +0530

    First commit

---
+++ src/add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class add {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			 System.out.println("Enter First Number to Print");
+			 int num1= sc.nextInt();
+			 System.out.println("Enter Secound Number to Print");
+			 int num2= sc.nextInt();
+			 System.out.println("Addition of "+num1+" and "+num2+" is "+(num1+num2));
+
+
+		}
+
+	}
